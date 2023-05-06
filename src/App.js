7730b2c4d81865fe3e3df4c2ff7f45b10f8bcff8
Minhajul Abedin <learnermoman@gmail.com>
2023-05-06 11:57:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Countreies from './components/Countries/Countreies';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Countreies></Countreies>
+      
     </div>
   );
 }
+
+
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch("https://restcountries.com/v3.1/all")
+//       .then(res => res.json())
+//       .then(data=>setCountries(data))
+//   },[])
+//   return (
+//     <div>
+//       <h1>Visiting Rest Countries !!!</h1>
+//       <h3>Available Countries :{countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population} capital={country.capital} ></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name:{props.name}</h2>
+//       <h3>Population:{props.population}</h3>
+//       <h5>Capital:{props.capital}</h5>
+     
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Countries/Countreies.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+
+const Countreies = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data=>setCountries(data))
+  },[])
+
+  return (
+    <div>
+      <h1>This is from Countries:{countries.length}</h1>
+      {
+        countries.map(country => <Country country={country} ></Country>)
+      }
+    </div>
+  );
+};
+
+export default Countreies;
